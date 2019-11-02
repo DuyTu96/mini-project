019a13a029763bb84ebf8055bf2392c1328bfa69
Duy Tu <duytuhubt@gmail.com>
2019-11-02 17:34:24 +0700

    complete controller and route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('admin.layouts.main');
+Route::group(['prefix' => 'admin'], function () {
+    //Categories
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('index','admin\CategoryController@index');
+        Route::get('add','admin\CategoryController@create');
+        Route::get('edit/{id}','admin\CategoryController@edit');
+    });
+    //User
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('index','admin\UserController@index');
+        Route::get('add','admin\UserController@create');
+        Route::get('edit/{id}','admin\UserController@edit');
+    });
+    //Product
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('index','admin\ProductController@index');
+        Route::get('add','admin\ProductController@create');
+        Route::get('edit/{id}','admin\ProductController@edit');
+    });
 });
