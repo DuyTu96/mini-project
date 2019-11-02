8341f266d74aeff3828f91a1ebe66db12327e8ef
Duy Tu <duytuhubt@gmail.com>
2019-11-02 18:03:58 +0700

    complete migration and model

---
+++ database/migrations/2019_11_02_103725_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100)->nullable();
+            $table->string('email', 100)->nullable()->unique();
+            $table->string('password', 100)->nullable();
+            $table->string('phone', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
